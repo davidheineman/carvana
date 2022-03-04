3d27aee4ca5ed56e083fbbc314c95b6cfa9bcb51
David <dheineman3@gatech.edu>
2022-03-04 20:01:38 +0100

    finish basic trie requirements

--- README.md
+++ README.md
@@ -5,13 +5,15 @@ This is my solution to the carvana coding challenge!
 - `gcc` compiler
 - If you want to test the naive solution, you need to have `jupyter` with `pandas` installed (`pip install jupyter pandas`)
 
-### How to run:
+### How to use
 - Clone this repository
 - Enter the 
-- Compile the trie with `gcc trie trie.cpp`
-- Run the trie with `./trie [filename to process]`
+- Compile the trie with `g++ trie.cpp -o trie`
+- Run the trie with `./trie [filename(s) to process (separated by spaces)]`
 
 *The repository contains data in `data/test_small.txt` and `data/test_tiny.txt`*
+
+*Example command: `g++ trie.cpp -o trie; ./trie ../data/test_small.txt ../data/test_small.txt`*
 
 ### How it works
 `naive.ipynb` contains python code on various naive solutions 
@@ -19,13 +21,15 @@ This is my solution to the carvana coding challenge!
 `trie.cpp` contains C++ code which creates a [trie](https://arxiv.org/pdf/1806.09447.pdf) using the corpus
     *This tree contains words at each node, making the tree more space efficient. It also scans the corpus at each line only one line of the file has to be loaded into memory at a time.*
 
-### What next:
+![tree](tree.jpg)
+
+### What's next
 I spent most of my time on efficiency, rather than meeting extra requirements. I would start by testing my code and building out the `trie.cpp` to have it support other applications than just counting pairs of words.
 
 Some test cases I haven't considered:
 - Non-unicode characters
 - Handling line-endings and small pairs of words
 - Handling words with a large number of characters
-- Handling large files with entirely un
+- Handling large files with entirely unique characters
 
 Also, I started `trieletters.cpp` as a more efficient implementation of `trie.cpp` which uses individual letters as nodes in the tree rather than entire words. This uses more primitive datatypes and less expensive C objects and would run faster. I ran into road blocks with `segmentation fault` errors, which is why the file is half-way done.
--- trie/trie.cpp
+++ trie/trie.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <unordered_map>
+#include <queue>
+#include <sstream>
 #include <string>
 #include <fstream>
 
@@ -11,8 +13,8 @@ const char delimiters[] = " \n\t";
 
 // Word trie node
 struct WordTrieNode {
-    std::string word;
-    std::unordered_map<std::string, WordTrieNode *> *children;
+    string word;
+    unordered_map<string, WordTrieNode *> *children;
 
     // TODO: Make sure count doesn't overflow
     int count;
@@ -20,20 +22,20 @@ struct WordTrieNode {
 
 struct WordTrieNode *createNode(void) {
     struct WordTrieNode *curr = new WordTrieNode;
-    curr->children = new std::unordered_map<std::string, WordTrieNode *>;
+    curr->children = new unordered_map<string, WordTrieNode *>;
     curr->count = 0;
 
     // TODO: Do I need to declare this?
     // curr->word = NULL;
 }
 
-void incrementNode(struct WordTrieNode *root, std::string *phrase) {
+void incrementNode(struct WordTrieNode *root, string *phrase) {
     struct WordTrieNode *curr = root;
 
     // Traverse to a leaf in the tree
-    std::string *currWord;
-    std::unordered_map<std::string, WordTrieNode *> *map;
-    std::unordered_map<std::string, WordTrieNode *>::iterator it;
+    string *currWord;
+    unordered_map<string, WordTrieNode *> *map;
+    unordered_map<string, WordTrieNode *>::iterator it;
     for (int i = 0; i < DEPTH; i++) {       
         // Get next word to traverse down the tree   
         currWord = &phrase[i];
@@ -52,7 +54,7 @@ void incrementNode(struct WordTrieNode *root, std::string *phrase) {
             newNode->word = *currWord;
 
             // Add new node to current node's children
-            map->insert(std::pair<std::string, WordTrieNode *>(newNode->word, newNode));
+            map->insert(pair<string, WordTrieNode *>(newNode->word, newNode));
 
             // Traverse to new node
             curr = newNode;
@@ -65,32 +67,31 @@ void incrementNode(struct WordTrieNode *root, std::string *phrase) {
 
 void printTree(WordTrieNode *root) {
     // TODO: Support tries with a depth of non-3
-    std::unordered_map<std::string, WordTrieNode *> *map = root->children;
-    std::unordered_map<std::string, WordTrieNode *>::iterator it;
+    unordered_map<string, WordTrieNode *> *map = root->children;
+    unordered_map<string, WordTrieNode *>::iterator it;
     for (it = map->begin(); it != map->end(); it++) {
-        std::unordered_map<std::string, WordTrieNode *> *map1 = it->second->children;
-        std::unordered_map<std::string, WordTrieNode *>::iterator it1;
+        unordered_map<string, WordTrieNode *> *map1 = it->second->children;
+        unordered_map<string, WordTrieNode *>::iterator it1;
         for (it1 = map1->begin(); it1 != map1->end(); it1++) {
-            std::unordered_map<std::string, WordTrieNode *> *map2 = it1->second->children;
-            std::unordered_map<std::string, WordTrieNode *>::iterator it2;
+            unordered_map<string, WordTrieNode *> *map2 = it1->second->children;
+            unordered_map<string, WordTrieNode *>::iterator it2;
             for (it2 = map2->begin(); it2 != map2->end(); it2++) {
                 if (it2->second->count > 3)
                     printf("%s %s %s: %d\n", it->second->word.data(), it1->second->word.data(), it2->second->word.data(), it2->second->count);
             }
         }
-        
     }
 }
 
 void basicTest(WordTrieNode *root) {
-    std::string pair1[3] = {"hello", "world", "i"};
-    std::string pair2[3] = {"hello", "world", "you"};
-    std::string pair3[3] = {"hello", "world", "i"};
-    std::string pair4[3] = {"why", "world", "i"};
-    std::string pair5[3] = {"you", "wouldnt", "even"};
-    std::string pair6[3] = {"you", "wouldnt", "even"};
-    std::string pair7[3] = {"you", "wouldnt", "even"};
-    std::string pair8[3] = {"you", "wouldnt", "dare"};
+    string pair1[3] = {"hello", "world", "i"};
+    string pair2[3] = {"hello", "world", "you"};
+    string pair3[3] = {"hello", "world", "i"};
+    string pair4[3] = {"why", "world", "i"};
+    string pair5[3] = {"you", "wouldnt", "even"};
+    string pair6[3] = {"you", "wouldnt", "even"};
+    string pair7[3] = {"you", "wouldnt", "even"};
+    string pair8[3] = {"you", "wouldnt", "dare"};
     
     incrementNode(root, pair1);
     incrementNode(root, pair2);
@@ -104,7 +105,7 @@ void basicTest(WordTrieNode *root) {
     WordTrieNode *one = root->children->find("why")->second;
     WordTrieNode *two = one->children->find("world")->second;
     int out = two->children->find("i")->second->count;
-    std::cout << (int) out << "\n";
+    cout << (int) out << "\n";
 }
 
 WordTrieNode *assembleTreeFromFile(char *filename) {
@@ -116,12 +117,12 @@ WordTrieNode *assembleTreeFromFile(char *filename) {
     handler.open(filename);
     if (handler.is_open()) {
         string line;
-        std::string currWord;
+        string currWord;
         size_t i;
         size_t j;
 
         // TODO: Need to handle edge cases better
-        std::string phrase[3] = {*(new std::string("Hello")), *(new std::string("Hello")), *(new std::string("Hello"))};
+        string phrase[3] = {*(new string("Hello")), *(new string("Hello")), *(new string("Hello"))};
         while (getline(handler, line)) {
             // Add each word in the line to the trie
             i = 0, j = 0;
@@ -146,22 +147,59 @@ WordTrieNode *assembleTreeFromFile(char *filename) {
     return root;
 }
 
-int main(int argc, char *argv[]) {
-    char* filename;
-    if (argc == 2) {
-        std::cout << "Processing file: " << argv[1] << "\n\n";
-        filename = argv[1];
-    } else {
-        filename = (char *) "test_small.txt";
-    }
-
+void printTopPairs(char* filename) {
     WordTrieNode *root = assembleTreeFromFile(filename);
 
-    // https://stackoverflow.com/questions/19227698/write-a-program-to-find-100-largest-numbers-out-of-an-array-of-1-billion-numbers
-    // to find the top 100 pairs:
-    // create a priority queue
-    // iterate through the tree
-    // add each phrase to the priority queue
+    // Create a priority queue to store values
+    priority_queue<pair<int, string>> pq;
 
-    printTree(root);
+    // Iterates through entire tree
+    unordered_map<string, WordTrieNode *> *map = root->children;
+    unordered_map<string, WordTrieNode *>::iterator it;
+    for (it = map->begin(); it != map->end(); it++) {
+        unordered_map<string, WordTrieNode *> *map1 = it->second->children;
+        unordered_map<string, WordTrieNode *>::iterator it1;
+        for (it1 = map1->begin(); it1 != map1->end(); it1++) {
+            unordered_map<string, WordTrieNode *> *map2 = it1->second->children;
+            unordered_map<string, WordTrieNode *>::iterator it2;
+            for (it2 = map2->begin(); it2 != map2->end(); it2++) {
+                // Add each phrase to the priority queue
+                stringstream ss;
+                ss << it->second->word.data() << " " << it1->second->word.data() << " " << it2->second->word.data();
+                string s = ss.str();
+
+                pq.push(make_pair(it2->second->count, s));
+
+                // If the priority queue is larger than 100, delete values
+                // TODO: Keep the priority queue below 100 values
+                // if (pq.size() > 100)
+                //     pq.pop();
+            }
+        }
+    }
+
+    // Print the top 100 values from the priority queue
+    // while (!pq.empty()) {
+    for (int i = 0; i < 100; i++) {
+        pair<int, string> next = pq.top();
+        cout << next.first << " : " << next.second << "\n";
+        pq.pop();
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // TODO: Process standard input if it exists
+    char* filename;
+    if (argc > 1) {
+        // Iterate through all filenames
+        for (int i = 1; i < argc; i++) {
+            cout << "Processing file: " << argv[i] << "\n\n";
+            filename = argv[i];
+            printTopPairs(filename);
+            cout << "\n";
+        }
+    } else {
+        cout << "Need to give filename as input!";
+    }   
+    return 0;
 }
